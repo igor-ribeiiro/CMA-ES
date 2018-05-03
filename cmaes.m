989b3f500cceec6dfb1d2bcc0cde14ce1a0c78eb
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-05-02 23:31:34 -0300

    Refactoring the load part

--- cmaes.m
+++ cmaes.m
@@ -21,22 +21,7 @@ function xmin=cmaes   % (mu/mu_w, lambda)-CMA-ES
   while counteval < stopeval
       numberOfLoops = numberOfLoops + 1;
       
-      
-      
-      % Generate and evaluate lambda offspring
-      [arx, arfitness, counteval] = loop_evaluate_function(lambda, xmean, sigma, B, D, N, strfitnessfct, counteval, arx, arfitness);
-      
-      % Sort by fitness and compute weighted mean into xmean
-      [arfitness, arindex, xold, xmean] = loop_sort_and_weight(arfitness, xmean, arx, weights, mu);
-      
-      % Cumulation: Update evolution paths
-      [ps, hsig, pc] = loop_update_evolution_paths(N, cs, ps, mueff, invsqrtC, xmean, xold, sigma, counteval, lambda, chiN, cc, pc);
-      
-      % Adapt covariance matrix C
-      C = loop_adapt_covariance(sigma, arx, arindex, xold, mu, c1, cmu, C, pc, hsig, cc, weights);
-      
-      % Adapt step size sigma
-      sigma = loop_adapt_sigma(sigma, cs, damps, ps, chiN);
+      [arx, arfitness, counteval, arindex, xmean, ps, pc, C, sigma] = loop_proccessing(lambda, xmean, sigma, B, C, D, N, strfitnessfct, counteval, arx, arfitness, weights, mu, cs, ps, mueff, invsqrtC, chiN, cc, pc, damps, c1, cmu);
       
       % Decomposition of C into B*diag(D.^2)*B' (diagonalization)
       if counteval - eigeneval > lambda/(c1+cmu)/N/10  % to achieve O(N^2)
